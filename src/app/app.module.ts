d8745c69803137840ba7b6f3e6309696223f37fa
Diana Soria <dianasoriati11d@gmail.com>
2023-10-02 18:30:24 -0600

    02/10/2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
+import{MatInputModule} from '@angular/material/input'
+import{MatButtonModule} from '@angular/material/button'
+import{MatIconModule} from '@angular/material/icon'
+import{MatToolbarModule} from '@angular/material/toolbar'
+
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
@@ -9,6 +14,9 @@ import { ProductoFilterPipe } from './product/producto-filter.pipe';
 import { OperasBasComponent } from './formularios/operas-bas/operas-bas.component';
 import { CalculosComponent } from './formularios/calculos/calculos.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SumaComponent } from './formularios/suma/suma.component';
+import { MenuComponent } from './menu/menu/menu.component';
+import { MaterialModule } from './material/material/material.module';
 
 @NgModule({
   declarations: [
@@ -16,12 +24,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ProductListComponent,
     ProductoFilterPipe,
     OperasBasComponent,
-    CalculosComponent
+    CalculosComponent,
+    SumaComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
+ 
     
   ],
   providers: [],
